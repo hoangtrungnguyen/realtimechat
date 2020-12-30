933d18105241f6725ac7cd9771945b94830f0ec0
Trung Nguyen Hoang <hoangtrungnguyen18102000@gmail.com>
2020-12-30 15:18:36 +0700

    Register view and login-logout

--- RealTimeChat/urls.py
+++ RealTimeChat/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -24,12 +24,10 @@ from personal.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_screen_view, name='home')
-
-
+    path('', home_screen_view, name='home'),
+    path('', include('account.urls'))
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
